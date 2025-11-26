5109e704d076e79e9591f59d4871b67da3849fb7
SubbaVyshnavi <vyshnavips371@gmail.com>
2025-11-26 11:38:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,5 +46,6 @@ if ing:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+st_df=st.dataframe(data=smoothiefroot_response.json())
 
